d8028c28dbe2af0ae6ad8dc638f9e96cd760276d
Ro-coder786 <roshankumar6824@gmail.com>
2025-10-08 08:48:11 +0530

    return server name dialog

--- MasterScheduler/ViewModels/SQLBackupScheduleViewModel.cs
+++ MasterScheduler/ViewModels/SQLBackupScheduleViewModel.cs
@@ -3,9 +3,11 @@ using CommunityToolkit.Mvvm.Input;
 using MasterScheduler.Interface;
 using MasterScheduler.Models;
 using MasterScheduler.Views;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +18,11 @@ namespace MasterScheduler.ViewModels
     public partial class SQLBackupScheduleViewModel : ObservableObject
     {
         private readonly INavigationService _navigationService;
+
+        [ObservableProperty]
+        private string serverName;
+
+        private string ConnectionString;
         public ObservableCollection<string> SelectedDatabases { get; set; } = new();
         public ObservableCollection<BackupDestination> BackupDestinations { get; set; } = new();
 
@@ -26,33 +33,6 @@ namespace MasterScheduler.ViewModels
             _navigationService = navigationService;
         }
 
-        [RelayCommand]
-        public void OpenDatabaseSelection()
-        {
-            var dialog = new DatabaseSelectionDialog();
-            dialog.Owner = Application.Current.Windows.OfType<Window>().SingleOrDefault(x=>x.IsActive);
-            // Simulate loading available databases
-            dialog.AvailableDatabases = new ObservableCollection<DatabaseItem>
-            {
-                new DatabaseItem { Name = "DB1" },
-                new DatabaseItem { Name = "DB2" },
-                new DatabaseItem { Name = "DB3" }
-            };
-            if (dialog.ShowDialog() == true)
-            {
-                SelectedDatabases.Clear();
-                foreach (var db in dialog.AvailableDatabases.Where(d => d.IsChecked))
-                {
-                    SelectedDatabases.Add(db.Name);
-                }
-            }
-        }
-
-        [RelayCommand]
-        public void BackupDestination()
-        {           
-            BackupDestinations.Add(new BackupDestination { Name = "DB1", Icon = "Server" });                        
-        }
 
         [RelayCommand]
         public void ConnectServer()
@@ -63,9 +43,52 @@ namespace MasterScheduler.ViewModels
 
             if (result == true)
             {
-                // Access dbSelectorVM.SelectedDatabases here
+                var vm =(MSSQLConnectViewModel)dialog.DataContext;
+                ServerName=vm.SelectedServer;
+                ConnectionString = $"Server={ServerName};Database=.;Trusted_Connection=True;TrustServerCertificate=True"; ;
             }
         }
+        [RelayCommand]
+        public void OpenDatabaseSelection()
+        {
+            var dialog = new DatabaseSelectionDialog();
+            dialog.Owner = Application.Current.Windows.OfType<Window>().SingleOrDefault(x=>x.IsActive);
+            // Simulate loading available databases
+            var databases = LoadDatabases();
+            dialog.AvailableDatabases = new ObservableCollection<DatabaseItem>(databases);
+            if (dialog.ShowDialog() == true)
+            {
+                SelectedDatabases.Clear();
+                foreach (var db in dialog.AvailableDatabases.Where(d => d.IsChecked))
+                {
+                    SelectedDatabases.Add(db.Name);
+                }
+            }
+        }
+
+        private IEnumerable<DatabaseItem> LoadDatabases()
+        {
+            var list = new List<DatabaseItem>();
+            using SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            var cmd = new SqlCommand("SELECT Name FROM sys.databases WHERE database_id > 4", connection); // exclude system DBs
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                list.Add(new DatabaseItem { Name = reader.GetString(0) });
+            }
+            return list;
+        }
+
+        [RelayCommand]
+        public void BackupDestination()
+        {           
+            BackupDestinations.Add(new BackupDestination { Name = "DB1", Icon = "Server" });                        
+        }
+
+        
 
         [RelayCommand]
         public void SchedulerSettings()
