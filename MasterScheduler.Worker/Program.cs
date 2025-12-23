526ad43f3996022ec21f23714cc6d92dacb3b4eb
Roshan Kumar <princekumar6824@gmail.com>
2025-12-23 17:04:16 +0530

    added a service lib and registered as windows service code

--- MasterScheduler.Worker/Program.cs
+++ MasterScheduler.Worker/Program.cs
@@ -1,8 +1,32 @@
 using MasterScheduler.Shared.Data;
 using MasterScheduler.Worker;
 
+//var builder = Host.CreateApplicationBuilder(args);
+//builder.Services.AddHostedService<Worker>();
+//DatabaseHelper.Initialize();
+//var host = builder.Build();
+//host.Run();
+
 var builder = Host.CreateApplicationBuilder(args);
+
+// IMPORTANT: Tell .NET this is a Windows Service
+
+//builder.Services.AddWindowsService(options =>
+//{
+//    options.ServiceName = "MasterScheduler";
+//});
+
+
+
+// Register worker
 builder.Services.AddHostedService<Worker>();
-DatabaseHelper.Initialize();
+
 var host = builder.Build();
+
+// Move init AFTER host is built
+using (var scope = host.Services.CreateScope())
+{
+    DatabaseHelper.Initialize();
+}
+
 host.Run();
